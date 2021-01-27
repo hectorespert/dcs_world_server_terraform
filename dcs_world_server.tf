958d2bd908d65eba35905e56a333d248140f2354
Hector Espert <hectorespertpardo@gmail.com>
2021-01-27 13:27:37 +0100

    Format disk

--- dcs_world_server.tf
+++ dcs_world_server.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "~> 2.70"
     }
   }
@@ -9,52 +9,103 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 data "aws_ami" "amazon_windows_2019_std" {
   most_recent = true
-  owners      = ["amazon"]
+  owners = [
+    "amazon"
+  ]
 
   filter {
-    name   = "name"
-    values = ["Windows_Server-2019-English-Full-Base-*"]
+    name = "name"
+    values = [
+      "Windows_Server-2019-English-Full-Base-*"
+    ]
   }
 }
 
 resource "aws_security_group" "rdp" {
 
   ingress {
-    from_port   = 3389
-    to_port     = 3389
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 3389
+    to_port = 3389
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
   }
 
   ingress {
-    from_port   = 3389
-    to_port     = 3389
-    protocol    = "tcp"
-    ipv6_cidr_blocks = ["::/0"]
+    from_port = 3389
+    to_port = 3389
+    protocol = "tcp"
+    ipv6_cidr_blocks = [
+      "::/0"
+    ]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    ipv6_cidr_blocks = ["::/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    ipv6_cidr_blocks = [
+      "::/0"
+    ]
   }
 }
 
+/*resource "aws_security_group" "winrm" {
+
+  ingress {
+    from_port = 5985
+    to_port = 5985
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  ingress {
+    from_port = 5985
+    to_port = 5985
+    protocol = "tcp"
+    ipv6_cidr_blocks = [
+      "::/0"
+    ]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    ipv6_cidr_blocks = [
+      "::/0"
+    ]
+  }
+}*/
+
 resource "tls_private_key" "rsa_key" {
-  algorithm   = "RSA"
+  algorithm = "RSA"
 }
 
 resource "aws_key_pair" "server_key" {
@@ -63,10 +114,40 @@ resource "aws_key_pair" "server_key" {
 
 resource "aws_instance" "dcs_world_server" {
   ami = data.aws_ami.amazon_windows_2019_std.image_id
-  instance_type = "c5ad.large"
-  security_groups = [aws_security_group.rdp.name]
+  instance_type = "c5ad.xlarge"
+  security_groups = [
+    aws_security_group.rdp.name,
+    //aws_security_group.winrm.name
+  ]
   key_name = aws_key_pair.server_key.key_name
   get_password_data = "true"
+  user_data = <<EOF
+<powershell>
+Initialize-Disk -Number 1 -PartitionStyle "GPT"
+New-Partition -DiskNumber 1 -UseMaximumSize -AssignDriveLetter
+Format-Volume -DriveLetter D -Confirm:$FALSE
+</powershell>
+EOF
+
+  /*connection {
+      type = "winrm"
+      port = 5986
+      host = self.public_dns
+      user = "Administrator"
+      password = rsadecrypt(self.password_data, tls_private_key.rsa_key.private_key_pem)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo Current date and time >> %SystemRoot%\\Temp\\test.log"
+    ]
+  }
+
+  provisioner "file" {
+    source = "test.txt"
+    destination = "C:/test.txt"
+  }*/
+
 }
 
 output "server-dns" {
@@ -74,9 +155,9 @@ output "server-dns" {
 }
 
 output "password_data" {
-  value= aws_instance.dcs_world_server.password_data
+  value = aws_instance.dcs_world_server.password_data
 }
 
 output "password_decrypted" {
-  value=rsadecrypt(aws_instance.dcs_world_server.password_data, tls_private_key.rsa_key.private_key_pem)
+  value = rsadecrypt(aws_instance.dcs_world_server.password_data, tls_private_key.rsa_key.private_key_pem)
 }
