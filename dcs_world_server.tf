87a5d7ce2aebc001a6e9d28898783552a8a9593d
Hector Espert <hectorespertpardo@gmail.com>
2021-01-27 11:09:59 +0100

    Administrator password

--- dcs_world_server.tf
+++ dcs_world_server.tf
@@ -12,7 +12,71 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+data "aws_ami" "amazon_windows_2019_std" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2019-English-Full-Base-*"]
+  }
+}
+
+resource "aws_security_group" "rdp" {
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "tls_private_key" "rsa_key" {
+  algorithm   = "RSA"
+}
+
+resource "aws_key_pair" "server_key" {
+  public_key = tls_private_key.rsa_key.public_key_openssh
+}
+
+resource "aws_instance" "dcs_world_server" {
+  ami = data.aws_ami.amazon_windows_2019_std.image_id
+  instance_type = "c5ad.large"
+  security_groups = [aws_security_group.rdp.name]
+  key_name = aws_key_pair.server_key.key_name
+  get_password_data = "true"
+}
+
+output "server-dns" {
+  value = aws_instance.dcs_world_server.public_dns
+}
+
+output "password_data" {
+  value= aws_instance.dcs_world_server.password_data
+}
+
+output "password_decrypted" {
+  value=rsadecrypt(aws_instance.dcs_world_server.password_data, tls_private_key.rsa_key.private_key_pem)
 }
